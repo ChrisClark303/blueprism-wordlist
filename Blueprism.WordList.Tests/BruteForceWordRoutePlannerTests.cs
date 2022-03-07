3b5f158988e2ba20cd2a41c3d7ef2f2eb9c05182
ChrisClark303 <38655477+ChrisClark303@users.noreply.github.com>
2022-03-07 13:59:13 +0000

    Moved some items around

--- Blueprism.WordList.Tests/BruteForceWordRoutePlannerTests.cs
+++ Blueprism.WordList.Tests/BruteForceWordRoutePlannerTests.cs
@@ -3,19 +3,7 @@ using NUnit.Framework;
 
 namespace Blueprism.WordList.Tests
 {
-    public class SingleLetterMergeWordRoutePlannerTest
-    {
-        [TestCase("spin", "spot", new[] { "spin", "spit", "spat", "spot", "span" }, new[] { "spin", "spit", "spot" })]
-        [TestCase("spin", "slur", new[] { "spin", "spit", "spat", "spot", "span", "spun", "spur", "slur" }, new string[0])]
-        public void PlanRouteBetweenWords_CorrectlyGeneratesWordList(string startWord, string endWord, string[] source, string[] expected)
-        {
-            var routePlanner = new SingleLetterMergeWordRoutePlanner();
-            var wordRoute = routePlanner.PlanRouteBetweenWords(source, startWord, endWord);
-            Assert.AreEqual(expected, wordRoute);
-        }
-    }
-
-    public class IncrementalWordRoutePlannerTest
+    public class BruteForceWordRoutePlannerTests
     {
         [TestCase("spin", "spot", new[] { "spin", "spit", "spat", "spot", "span" }, new[] { "spin", "spit", "spot" })]
         [TestCase("spin", "slur", new[] { "spin", "spit", "spat", "spot", "span", "spun", "spur", "slur" }, new[] { "spin", "spun", "spur", "slur" })]
---
+++ Blueprism.WordList.Tests/SingleLetterMergeWordRoutePlannerTests.cs
@@ -0,0 +1,17 @@
+﻿using Blueprism.Wordlist.Core;
+using NUnit.Framework;
+
+namespace Blueprism.WordList.Tests
+{
+    public class SingleLetterMergeWordRoutePlannerTests
+    {
+        [TestCase("spin", "spot", new[] { "spin", "spit", "spat", "spot", "span" }, new[] { "spin", "spit", "spot" })]
+        [TestCase("spin", "slur", new[] { "spin", "spit", "spat", "spot", "span", "spun", "spur", "slur" }, new string[0])]
+        public void PlanRouteBetweenWords_CorrectlyGeneratesWordList(string startWord, string endWord, string[] source, string[] expected)
+        {
+            var routePlanner = new SingleLetterMergeWordRoutePlanner();
+            var wordRoute = routePlanner.PlanRouteBetweenWords(source, startWord, endWord);
+            Assert.AreEqual(expected, wordRoute);
+        }
+    }
+}
